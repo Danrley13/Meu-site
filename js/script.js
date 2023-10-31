93242c761e8d8c5bbfbfa457c32a54d8720307cb
Danrley Lucas santos silva <140899150+Danrley13@users.noreply.github.com>
2023-10-31 20:47:28 -0300

    meu-site 31/10

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// tres (3) constantes para referencia as items divs de projetos 
-
 const album = document.getElementById('album');
+const casamentos = document.getElementById('casamentos');
+const buffet = document.getElementById('buffet');
+
+const videoAlbum = document.querySelector('#videoAlbum');
+const videoCasamentos = document.querySelector('#videoCasamentos');
+const videoBuffet = document.querySelector('#videoBuffet');
 
 
-// tres (3) constants para referencia os id's do iFrames
-const videoAlbum = document.querySelector('#videoAlmbum');
-
-//evento click 
 album.addEventListener('click', () => {
     album.classList.toggle('active');
     videoAlbum.classList.toggle('active');
 });
-
+casamentos.addEventListener('click', () => {
+    casamentos.classList.toggle('active');
+    videoCasamentos.classList.toggle('active');
+});
+buffet.addEventListener('click', () => {
+    buffet.classList.toggle('active');
+    videoBuffet.classList.toggle('active');
+});
